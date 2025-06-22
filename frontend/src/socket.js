ab1457ca9c6c55d8b88521f8aa11951f14011ca8
Aryan Paul <118630704+AryanPaul3@users.noreply.github.com>
2025-06-22 21:16:05 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const URL = import.meta.env.MODE ==='development' ? "http://localhost:5000" : "/";
+const socket = io(URL, {
     autoConnect: false,
   withCredentials: true,
 });
